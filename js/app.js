ac37680f29b66597ef113d73c15fbe9cb45ef2ef
Samantha Hirsch <sammyhirsch0109@gmail.com>
2024-05-15 13:06:51 -0400

    adjacent html

--- js/app.js
+++ js/app.js
@@ -3,7 +3,17 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 
 const list = document.getElementById("list");
+function addToDo (toDo) {
+const text = `<li class="item">
+<i class="fa fa-circle-thin" job="complete"></i>
+<p class="text"> ${toDo} </p>
+<i class="fa fa-trash-o" job="delete"> </i>
+</li>`
 
+const position = "beforeend";
+list.insertAdjacentHTML(position, text);
+}
 const input = document.getElementById("input")
 
 const element = document.getElementById("element")
+
