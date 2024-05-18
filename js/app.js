b1e99606a168b5bca03c9e47056ddfbd00c45cc1
samhirsch0109 <144719425+samhirsch0109@users.noreply.github.com>
2024-05-18 23:30:24 +0000

    update to do

--- js/app.js
+++ js/app.js
@@ -3,16 +3,30 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 
 const list = document.getElementById("list");
-function addToDo (toDo) {
+
+
+const CHECK = "fa-check-circle";
+const UNCHECK ="fa-circle-thin";
+const LINE_THROUGH = "lineThrough"
+function addToDo (toDo ,id,done, trash) {
+
+if(trash){ return; }
+    const DONE = done ? Check : UNCHECK;
+    const LINE = done ? LINE_THROUGH: "";
+
+
 const text = `<li class="item">
-<i class="fa fa-circle-thin" job="complete"></i>
-<p class="text"> ${toDo} </p>
-<i class="fa fa-trash-o" job="delete"> </i>
+<i class="fa ${Done} complete" job="complete" id="${id}"></i>
+<p class="text" ${LINE}> ${toDo} </p>
+<i class="fa fa-trash-o" job="delete" id=${id}"> </i>
 </li>`
 
 const position = "beforeend";
 list.insertAdjacentHTML(position, text);
 }
+
+addToDo("Drink Coffee") 
+
 const element = document.getElementById("element")
 
 const input = document.getElementById("input");
@@ -21,20 +35,37 @@ document.addEventListener("keyup",function(event){
     if( event.keyCode == 13){
         const toDo = input.value;
         if(toDo){
-            addToDo(toDo)
+            addToDo(toDo,id, false, false)
+            LIST.push(
+                {
+                    name: toDo,
+                    id: id,
+                    done: false,
+                    trash:false
+                }
+            )
         }
         input.value = "";
+        id++;
     }
 });
 
-let LIST = []
+let LIST = [];
 let id = 0;
 
-LIST[0] ->
+LIST =[{} ,{} , ];
+LIST[0] -->
 {
-    name : "Drink Coffe",
-    id : 0,
-    done : false,
-    trash : false
+    name: "Drink Coffee",
+    id: 0,
+    done: false,
+    trash: false
 }
-LIST[1] ->
+
+LIST[1] -->
+{
+    name: "Workout",
+    id: 1,
+    done: true,
+    trash: false
+}
